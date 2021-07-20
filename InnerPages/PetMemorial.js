65a2ec5768684acbb03eb90359a1cfd64f72a09b
Advocator ltd <info.advocator@gmail.com>
2021-07-20 21:33:45 +0500

    Pet Plaque inner page

--- InnerPages/PetMemorial.js
+++ InnerPages/PetMemorial.js
@@ -1,47 +1,80 @@
 gsap.registerPlugin(ScrollTrigger);
 
 ScrollTrigger.defaults({
-    toggleActions:"play  none none complete"
+    toggleActions: "play none none complete"
 });
-let tl2=gsap.timeline({
 
-});
-    var tl = gsap.timeline(
-        {
-            scrollTrigger: {
-                trigger: ".banner__next__content",
-                start: "top center",
-            },
+let tl = gsap.timeline(
+    {
+        scrollTrigger: {
+            trigger: "#banner__next__content",
+            start: "top center",
+        },
+        repeat: 0
+    }
+);
+let t2 = gsap.timeline(
+    {
+        scrollTrigger: {
+            trigger: "#banner__two__next__content",
+            start: "top center",
+        },
+        repeat: 0
+    }
+);
+let t3 = gsap.timeline(
+    {
+        scrollTrigger: {
+            trigger: "#banner__three__next__content",
+            start: "top center",
+        },
+        repeat: 0
+    }
+);
+tl.from("#border__bottom", {
+    x: -50,
+    opacity: 0,
+    duration: 1
+})
+    .from("#banner__next__header", {
+        y: 30,
+        opacity: 0,
+        duration: 1
+    }, "-=1")
+    .from("#banner__next__text", {
+        y: 30,
+        opacity: 0,
+        duration: 1
+    }, "-=1")
+t2.from("#border__bottom_two", {
+    x: -50,
+    opacity: 0,
+    duration: 1
+})
+    .from("#banner__two__next__header", {
+        y: 30,
+        opacity: 0,
+        duration: 1
+    }, "-=1")
+    .from("#banner__two__next__text", {
+        y: 30,
+        opacity: 0,
+        duration: 1
+    }, "-=1")
+t3.from("#border__bottom__three", {
+    x: -50,
+    opacity: 0,
+    duration: 1
+})
+    .from("#banner__three__next__header", {
+        y: 30,
+        opacity: 0,
+        duration: 1
+    }, "-=1")
+    .from("#banner__three__next__text", {
+        y: 30,
+        opacity: 0,
+        duration: 1
+    }, "-=1")
 
-    repeat: 0
-
-}
-    );
-    tl.from(".border__bottom",{
-        x:-50,
-        opacity:0,
-        duration:1
-    })
-        .from("#banner__next__header",{
-            y:30,
-            opacity:0,
-            duration:1
-        },"-=1")
-        .from("#banner__next__text",{
-            y:30,
-            opacity:0,
-            duration:1
-        },"-=1")
-    //
-    // tl.to(".cont", {
-    //     duration: 1,
-    //     text: {
-    //         value: "Do you really have corona",     //New text
-    //         newClass: "class2",                     //New class for the new text
-    //         delimiter: " "                          //changes text word by word can be removed to change the whole sentence
-    //     }
-    // });
-    // tl.from(".border__bottom", {scaleX: 0, transformOrigin: "right center"});
-    // tl.from("#banner__next__header", {duration: 1.00, y: 30}, "text");
-    // tl.from("#banner__next__text", {duration: 1.00, y: -30}, "text");
 
